8118d12a48c170c511dd4dcf3a07f81fa5df3893
kristofszigeti <kristofszigeti1@gmail.com>
2025-10-07 12:47:17 +0200

    Lab04_BestPlaces_iOSApp

---
+++ Lab04/BestPlaces/BestPlaces/BestPlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestPlacesApp.swift
+//  BestPlaces
+//
+//  Created by kristof on 2025. 10. 07..
+//
+
+import SwiftUI
+
+@main
+struct BestPlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
